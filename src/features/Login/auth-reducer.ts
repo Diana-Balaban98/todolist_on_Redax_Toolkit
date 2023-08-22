e189903963a18941db81467094fa56c7e671e422
Diana-Balaban98 <diana.rud.2012@mail.ru>
2023-08-22 19:42:19 +0300

    writing a slice for app

--- src/features/Login/auth-reducer.ts
+++ src/features/Login/auth-reducer.ts
@@ -1,4 +1,4 @@
-import { setAppStatusAC } from "../../app/app-reducer";
+import { appActions } from "../../app/app-reducer";
 import { authAPI, LoginParamsType } from "../../api/todolists-api";
 import { handleServerAppError, handleServerNetworkError } from "../../utils/error-utils";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
@@ -23,13 +23,13 @@ export const authActions = slice.actions;
 export const loginTC =
   (data: LoginParamsType): AppThunk =>
   (dispatch) => {
-    dispatch(setAppStatusAC("loading"));
+    dispatch(appActions.setAppStatus({ status: "loading" }));
     authAPI
       .login(data)
       .then((res) => {
         if (res.data.resultCode === 0) {
           dispatch(authActions.setIsLoggedIn({ isLoggedIn: true }));
-          dispatch(setAppStatusAC("succeeded"));
+          dispatch(appActions.setAppStatus({ status: "succeeded" }));
         } else {
           handleServerAppError(res.data, dispatch);
         }
@@ -39,13 +39,13 @@ export const loginTC =
       });
   };
 export const logoutTC = (): AppThunk => (dispatch) => {
-  dispatch(setAppStatusAC("loading"));
+  dispatch(appActions.setAppStatus({ status: "loading" }));
   authAPI
     .logout()
     .then((res) => {
       if (res.data.resultCode === 0) {
         dispatch(authActions.setIsLoggedIn({ isLoggedIn: false }));
-        dispatch(setAppStatusAC("succeeded"));
+        dispatch(appActions.setAppStatus({ status: "succeeded" }));
       } else {
         handleServerAppError(res.data, dispatch);
       }
